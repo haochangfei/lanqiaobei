1a0452041027f1039a1228a0d50a5bad44293679
haochangfei <34223852+haochangfei@users.noreply.github.com>
2017-12-19 18:54:30 +0800

    Add files via upload

---
+++ BASIC-11.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char sz[10];
+    cin>>sz;
+    long long n = 0;
+    sscanf(sz,"%x",&n);
+    printf("%lld\n",n);
+    return 0;
+}
